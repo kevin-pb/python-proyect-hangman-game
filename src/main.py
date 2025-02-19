72cacd7c8f4cf5d421b2314b4841f13a5d4e9a93
USER <kevinpb.py@gmail.com>
2025-02-19 22:33:22 +0100

    ad

--- src/main.py
+++ src/main.py
@@ -1,11 +1,7 @@
 # Importing modules-----------------
-from libs.workWithWords import checking_concordances as ch_c
-from libs.workWithWords import convert_list_to_str as l_to_s
 from libs.workWithWords import random_word_selector as r_word_selector
 from libs.workWithWords import wordSyllablesQuantity
-from libs.dbOperations import read_db
 from libs.dbOperations import read_db_and_convert_to_list
-from libs.dbOperations import db_agregate_one_word
 from libs.uiInterface import uiInterface
 # Importing modules-----------------
 
